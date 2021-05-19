ecd3b009e2aeeb548d12d8638cdb1ce7a9cb2d88
KrystianKierklo <73948612+KrystianKierklo@users.noreply.github.com>
2021-05-19 20:50:48 +0200

    Matplotilb2

---
+++ Matplotlib_2/main.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.axes3d import get_test_data
+from matplotlib import cm
+
+# # zadanie 1
+# fig1 = plt.figure()
+# ax = fig1.gca(projection = "3d")
+# z = np.linspace(0, 2*np.pi, 100)
+# x = np.sin(z)
+# y = 2*np.cos(z)
+# ax.plot(x, y, z, label='Wykres z zadania 1')
+# plt.show()
+#
+# # zadanie 2
+# np.random.seed( 19680801 )
+# def randrange(n, vmin, vmax):
+#     return (vmax - vmin) * np.random.rand(n) + vmin
+#
+# fig2 = plt.figure()
+# ax2 = fig2.add_subplot(111, projection = "3d")
+#
+# x1 = randrange(100, 0, 100)
+# x2 = randrange(100, 10, 55)
+# x3 = randrange(100, 3, 77)
+# x4 = randrange(100, 2, 90)
+# x5 = randrange(100, 5, 65)
+#
+# y1 = randrange(100, 6, 20)
+# y2 = randrange(100, 10, 12)
+# y3 = randrange(100, 15, 34)
+# y4 = randrange(100, 18, 97)
+# y5 = randrange(100, 55, 66)
+#
+# z1 = randrange(100, 10, 100)
+# z2 = randrange(100, 40, 78)
+# z3 = randrange(100, 3, 99)
+# z4 = randrange(100, 15, 60)
+# z5 = randrange(100, 12, 86)
+#
+# ax2.scatter(x1, y2, z3, c="aqua", marker="P", label="pierwszy")
+# ax2.scatter(x2, y2, z2, c="brown", marker="X", label="drugi")
+# ax2.scatter(x3, y3, z3, c="darkorange", marker="o", label="trzeci")
+# ax2.scatter(x4, y4, z4, c="lime", marker=4, label="czwarty")
+# ax2.scatter(x5, y5, z5, c="crimson", marker="d", label="piaty")
+#
+# plt.title("Wykres do zadania 2")
+# plt.legend()
+# plt.show()
+
+# zadanie 3
+
+
+# zadanie 4
+
+# zadanie 5
